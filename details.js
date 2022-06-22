1303ab3c6d0d6b11513e3b61060e34148f090632
Pearl Okoroafor <pearlygatess01@gmail.com>
2022-06-22 21:27:26 +0100

    Initial commit

---
+++ details.js
@@ -0,0 +1,5 @@
+let person ="Pearl Okoroafor";
+let height = 5.5;
+let country = "Nigeria";
+
+alert('My name is '+ person +'.\n My height is '+ height + ', and the name of my country is ' + country +'.');
